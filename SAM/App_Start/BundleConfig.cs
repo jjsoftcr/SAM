84accedceaec8e6cce8b37304828f8dec8545915
javierxbrenes1 <javierxbrenes@outlook.es>
2018-06-25 22:38:13 +0200

    Se agrega la plantilla al layout y se separa el navbar en otro archivo

--- SAM/App_Start/BundleConfig.cs
+++ SAM/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace SAM
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            /*********************** bundles template ************************************/
+            
         }
     }
 }
